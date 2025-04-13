e321f9aeb86b5a30be5501284121c68f3d81d975
PikuzaMarie <frag1lemaster@proton.me>
2025-04-13 17:26:47 +0300

    feat: create AccountToggle component

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,3 +1,11 @@
+import { AccountToggle } from './AccountToggle';
+
 export const Sidebar: React.FC = () => {
-  return <div></div>;
+  return (
+    <div>
+      <div className="sticky top-4 h-[calc(100vh-32px-48px)] overflow-y-scroll">
+        <AccountToggle />
+      </div>
+    </div>
+  );
 };

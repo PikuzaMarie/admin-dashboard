20061e8811a521ee3dd57f3d4fa162e3d8301830
PikuzaMarie <frag1lemaster@proton.me>
2025-04-14 19:48:48 +0300

    feat: change action to "see details"

--- src/pages/Products/index.tsx
+++ src/pages/Products/index.tsx
@@ -1,6 +1,6 @@
 import { motion } from 'framer-motion';
 import React, { useState } from 'react';
-import { FiDelete, FiEdit3, FiStar } from 'react-icons/fi';
+import { FiEye, FiStar } from 'react-icons/fi';
 import { useLoaderData } from 'react-router-dom';
 
 import { Dashboard } from '../../components/Dashboard';
@@ -108,13 +108,16 @@ export const ProductsPage: React.FC = () => {
                   {product.price}
                 </td>
                 <td className="px-2 py-2 text-xs text-stone-800">
-                  <div className="flex items-center gap-1">
-                    <button className="mr-2 rounded-sm p-1 hover:bg-purple-100">
-                      <FiEdit3 size={18} color={COLORS.purple} />
+                  <div className="flex items-center gap-0.5">
+                    <button
+                      className="mr-2 rounded-sm p-1 hover:bg-pink-100"
+                      aria-describedby="hint-id"
+                    >
+                      <FiEye size={18} color={COLORS.purple} />
                     </button>
-                    <button className="mr-2 rounded-sm p-1 hover:bg-pink-100">
-                      <FiDelete size={18} color={COLORS.pink} />
-                    </button>
+                    <p id="hint-id" className="textstone-800 text-xs">
+                      See details
+                    </p>
                   </div>
                 </td>
               </motion.tr>

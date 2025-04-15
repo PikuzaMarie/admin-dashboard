f860b96ba8d4471f256dff4b4cf553fa9f146987
PikuzaMarie <frag1lemaster@proton.me>
2025-04-15 23:01:11 +0300

    feat: dispatch user fetching action

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
 import { RouterProvider } from 'react-router-dom';
 
+import { fetchUsers } from './features/users/usersSlice';
+import { useAppDispatch } from './hooks';
 import { routes } from './pages/router';
 
 function App() {
+  const dispatch = useAppDispatch();
+  dispatch(fetchUsers());
+
   return <RouterProvider router={routes} />;
 }
 

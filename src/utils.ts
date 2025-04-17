52317c2c8b09ed4fb33be2a87554b986a3f78dfc
PikuzaMarie <frag1lemaster@proton.me>
2025-04-18 00:22:54 +0300

    feat: create validation functions for pagination params

---
+++ src/utils.ts
@@ -0,0 +1,11 @@
+import { ITEMS_PER_PAGE_OPTIONS } from './constants';
+
+export function validatePage(newPage: number, totalPages: number) {
+  return Math.max(1, Math.min(newPage, totalPages));
+}
+
+export function validateItemsPerPage(newValue: number) {
+  return ITEMS_PER_PAGE_OPTIONS.includes(newValue)
+    ? newValue
+    : ITEMS_PER_PAGE_OPTIONS[0];
+}

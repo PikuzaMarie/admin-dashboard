2a75cdc3b6839d3d9cda70924802a918c63995d8
PikuzaMarie <frag1lemaster@proton.me>
2025-04-17 13:24:52 +0300

    feat: create ProductsTable component

---
+++ src/components/ProductsTable.tsx
@@ -0,0 +1,97 @@
+import { motion } from 'framer-motion';
+import React from 'react';
+import { FiEye, FiStar } from 'react-icons/fi';
+
+import { COLORS, PRODUCTS_TABLE_HEADERS } from '../constants';
+import { Product } from '../types';
+import { getCategoryColor } from '../utils/getCategoryColorName';
+
+interface ProductsTableProps {
+  productsData: Product[];
+}
+
+export const ProductsTable: React.FC<ProductsTableProps> = ({
+  productsData,
+}) => {
+  return (
+    <table className="min-w-full border-y-1 border-stone-300">
+      <thead className="border-b-1 border-stone-300">
+        <tr>
+          {Object.values(PRODUCTS_TABLE_HEADERS).map(header => (
+            <th
+              key={header}
+              className="px-2 py-2 text-left text-xs font-medium tracking-wider text-stone-500 uppercase"
+            >
+              {header}
+            </th>
+          ))}
+          <th className="px-2 py-2 text-left text-xs font-medium tracking-wider text-stone-500 uppercase">
+            Actions
+          </th>
+        </tr>
+      </thead>
+      <tbody className="divide-y divide-stone-200">
+        {productsData.map(product => (
+          <motion.tr
+            key={product.id}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 0.3 }}
+          >
+            <td className="px-2 py-2 text-xs font-medium whitespace-nowrap text-stone-800">
+              {product.id}
+            </td>
+            <td className="px-2 py-2 text-xs font-medium whitespace-nowrap text-stone-800">
+              {product.sku}
+            </td>
+            <td className="block w-25 px-2 py-2 text-xs font-medium whitespace-nowrap">
+              <img src={product.thumbnail} alt={product.title} />
+            </td>
+            <td className="max-w-[100px] overflow-hidden px-2 py-2 text-xs font-medium text-ellipsis whitespace-nowrap text-stone-800">
+              {product.title}
+            </td>
+            <td className="max-w-[200px] overflow-hidden px-2 py-2 text-xs font-medium text-ellipsis whitespace-nowrap text-stone-800">
+              {product.description.substring(0, 60)}
+            </td>
+            <td className="max-w-[200px] overflow-hidden px-2 py-2 text-xs font-medium text-ellipsis whitespace-nowrap text-stone-800">
+              {product.brand}
+            </td>
+            <td className="px-2 py-2 text-xs whitespace-nowrap text-stone-800">
+              <span
+                className={`inline-flex rounded-full px-2 text-xs leading-5 font-semibold bg-${getCategoryColor(product.category)}-100 text-${getCategoryColor(product.category)}-800`}
+              >
+                {product.category}
+              </span>
+            </td>
+            <td className="px-2 py-2 text-xs whitespace-nowrap text-stone-800">
+              {product.price}
+            </td>
+            <td className="px-2 py-2 text-xs whitespace-nowrap text-stone-800">
+              {product.stock}
+            </td>
+            <td className="px-2 py-2 text-xs text-stone-800">
+              <div className="flex items-center gap-1">
+                <FiStar fill={COLORS.yellow} stroke={COLORS.yellow} />
+                {product.rating}
+              </div>
+            </td>
+
+            <td className="px-2 py-2 text-xs text-stone-800">
+              <div className="flex items-center gap-0.5">
+                <button
+                  className="mr-2 rounded-sm p-1 hover:bg-pink-100"
+                  aria-describedby="hint-id"
+                >
+                  <FiEye size={18} color={COLORS.purple} />
+                </button>
+                <p id="hint-id" className="textstone-800 text-xs">
+                  See details
+                </p>
+              </div>
+            </td>
+          </motion.tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};

f86bc27e1232029dd018d16d1685d1e877e2cfbe
PikuzaMarie <frag1lemaster@proton.me>
2025-04-18 13:05:35 +0300

    feat: create util function to validate pagination params

--- src/utils.ts
+++ src/utils.ts
@@ -9,3 +9,21 @@ export function validateItemsPerPage(newValue: number) {
     ? newValue
     : ITEMS_PER_PAGE_OPTIONS[0];
 }
+
+export function validatePaginationParams(
+  currentPage: number,
+  itemsPerPage: number,
+  itemsTotal: number,
+) {
+  const validatedItemsPerPage = validateItemsPerPage(itemsPerPage);
+  let validatedCurrentPage;
+
+  if (itemsTotal > 0) {
+    const totalPages = Math.ceil(itemsTotal / Number(validatedItemsPerPage));
+    validatedCurrentPage = validatePage(currentPage, totalPages);
+  } else {
+    validatedCurrentPage = 1;
+  }
+
+  return { validatedItemsPerPage, validatedCurrentPage };
+}

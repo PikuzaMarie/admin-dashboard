c9414d21faec7734757c40f3ccd42f5dc59b9e2a
PikuzaMarie <frag1lemaster@proton.me>
2025-04-18 23:11:10 +0300

    style: adjust header and main styles

--- src/components/Dashboard/index.tsx
+++ src/components/Dashboard/index.tsx
@@ -6,10 +6,10 @@ interface DashboardProps {
 export const Dashboard: React.FC<DashboardProps> = ({ title, children }) => {
   return (
     <div className="h-auto w-full rounded-lg bg-white pb-4 shadow">
-      <header className="mt-2 mb-4 border-b border-stone-200 px-4 pb-4">
+      <header className="mt-2 mb-4 border-b border-stone-200 px-4 pb-7">
         <h1 className="block text-lg font-bold">{title}</h1>
       </header>
-      <main className="flex flex-col gap-4 px-4 pb-4">{children}</main>
+      <main className="flex flex-col gap-4 px-4">{children}</main>
     </div>
   );
 };

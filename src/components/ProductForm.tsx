a7951b58a5608f88375996022d1219add010508c
PikuzaMarie <frag1lemaster@proton.me>
2025-04-23 19:30:16 +0300

    feat: replace sku with category field

--- src/components/ProductForm.tsx
+++ src/components/ProductForm.tsx
@@ -1,13 +1,14 @@
 import React, { FormEvent } from 'react';
 
-import { Product } from '../types';
+import { CATEGORIES } from '../constants';
+import { Category, Product } from '../types';
 
 interface EditProductPageFormFileds extends HTMLFormControlsCollection {
-  sku: HTMLInputElement;
   title: HTMLInputElement;
   description: HTMLInputElement;
   thumbnail: HTMLInputElement;
   brand: HTMLInputElement;
+  category: HTMLSelectElement;
   price: HTMLInputElement;
   stock: HTMLInputElement;
 }
@@ -29,19 +30,20 @@ export const ProductForm: React.FC<ProductFormProps> = ({
     e.preventDefault();
 
     const { elements } = e.currentTarget;
-    const sku = elements.sku.value;
+
     const title = elements.title.value;
     const description = elements.description.value;
     const thumbnail = elements.thumbnail.value;
+    const category = elements.category.value as Category;
     const brand = elements.brand.value;
     const price = +elements.price.value;
     const stock = +elements.stock.value;
 
     const updatedProduct = {
-      sku,
       title,
       description,
       thumbnail,
+      category,
       brand,
       price,
       stock,
@@ -52,22 +54,6 @@ export const ProductForm: React.FC<ProductFormProps> = ({
 
   return (
     <form onSubmit={handleSubmit} className="flex flex-col gap-4">
-      <p className="flex flex-col gap-2">
-        <label
-          htmlFor="sku"
-          className="block text-sm/6 font-medium text-stone-900"
-        >
-          Sku
-        </label>
-        <input
-          type="text"
-          id="sku"
-          name="sku"
-          defaultValue={product?.sku}
-          required
-          className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-stone-900 outline-1 -outline-offset-1 outline-stone-300 focus:outline-2 focus:-outline-offset-2 focus:outline-violet-600 sm:text-sm/6"
-        />
-      </p>
       <p className="flex flex-col gap-2">
         <label
           htmlFor="title"
@@ -116,6 +102,43 @@ export const ProductForm: React.FC<ProductFormProps> = ({
           required
           className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-stone-900 outline-1 -outline-offset-1 outline-stone-300 focus:outline-2 focus:-outline-offset-2 focus:outline-violet-600 sm:text-sm/6"
         />
+      </p>
+      <p className="flex flex-col gap-2">
+        <label
+          htmlFor="category"
+          className="block text-sm/6 font-medium text-stone-900"
+        >
+          Category
+        </label>
+        <div className="grid grid-cols-1">
+          <select
+            id="category"
+            name="category"
+            defaultValue={product?.category}
+            required
+            className="col-start-1 row-start-1 w-full appearance-none rounded-md bg-white py-1.5 pr-8 pl-3 text-base text-stone-900 outline-1 -outline-offset-1 outline-stone-300 focus:outline-2 focus:-outline-offset-2 focus:outline-violet-600 sm:text-sm/6"
+          >
+            <option value=""></option>
+            {CATEGORIES.map(category => (
+              <option key={category} value={category}>
+                {category}
+              </option>
+            ))}
+          </select>
+          <svg
+            className="pointer-events-none col-start-1 row-start-1 mr-2 size-5 self-center justify-self-end text-stone-500 sm:size-4"
+            viewBox="0 0 16 16"
+            fill="currentColor"
+            aria-hidden="true"
+            data-slot="icon"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M4.22 6.22a.75.75 0 0 1 1.06 0L8 8.94l2.72-2.72a.75.75 0 1 1 1.06 1.06l-3.25 3.25a.75.75 0 0 1-1.06 0L4.22 7.28a.75.75 0 0 1 0-1.06Z"
+              clip-rule="evenodd"
+            />
+          </svg>
+        </div>
       </p>
       <p className="flex flex-col gap-2">
         <label

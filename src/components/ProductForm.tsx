d3e24359f85f2157cb6f63089740422dd186caa3
PikuzaMarie <frag1lemaster@proton.me>
2025-04-23 18:47:02 +0300

    style: add styles for ProductForm component

--- src/components/ProductForm.tsx
+++ src/components/ProductForm.tsx
@@ -51,80 +51,133 @@ export const ProductForm: React.FC<ProductFormProps> = ({
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <p>
-        <label htmlFor="sku">Sku</label>
+    <form onSubmit={handleSubmit} className="flex flex-col gap-4">
+      <p className="flex flex-col gap-2">
+        <label
+          htmlFor="sku"
+          className="block text-sm/6 font-medium text-stone-900"
+        >
+          Sku
+        </label>
         <input
           type="text"
           id="sku"
           name="sku"
           defaultValue={product?.sku}
           required
+          className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-stone-900 outline-1 -outline-offset-1 outline-stone-300 focus:outline-2 focus:-outline-offset-2 focus:outline-violet-600 sm:text-sm/6"
         />
       </p>
-      <p>
-        <label htmlFor="title">Title</label>
+      <p className="flex flex-col gap-2">
+        <label
+          htmlFor="title"
+          className="block text-sm/6 font-medium text-stone-900"
+        >
+          Title
+        </label>
         <input
           type="text"
           id="title"
           name="title"
           defaultValue={product?.title}
           required
+          className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-stone-900 outline-1 -outline-offset-1 outline-stone-300 focus:outline-2 focus:-outline-offset-2 focus:outline-violet-600 sm:text-sm/6"
         />
       </p>
-      <p>
-        <label htmlFor="description">Description</label>
-        <input
-          type="text"
+      <p className="flex flex-col gap-2">
+        <label
+          htmlFor="description"
+          className="block text-sm/6 font-medium text-stone-900"
+        >
+          Description
+        </label>
+        <textarea
+          rows={2}
           id="description"
           name="description"
           defaultValue={product?.description}
           required
+          className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-stone-900 outline-1 -outline-offset-1 outline-stone-300 focus:outline-2 focus:-outline-offset-2 focus:outline-violet-600 sm:text-sm/6"
         />
       </p>
-      <p>
-        <label htmlFor="thumbnail">Thumbnail</label>
+
+      <p className="flex flex-col gap-2">
+        <label
+          htmlFor="thumbnail"
+          className="block text-sm/6 font-medium text-stone-900"
+        >
+          Thumbnail
+        </label>
         <input
           type="url"
           id="thumbnail"
           name="thumbnail"
           defaultValue={product?.thumbnail}
           required
+          className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-stone-900 outline-1 -outline-offset-1 outline-stone-300 focus:outline-2 focus:-outline-offset-2 focus:outline-violet-600 sm:text-sm/6"
         />
       </p>
-      <p>
-        <label htmlFor="brand">Brand</label>
+      <p className="flex flex-col gap-2">
+        <label
+          htmlFor="brand"
+          className="block text-sm/6 font-medium text-stone-900"
+        >
+          Brand
+        </label>
         <input
           type="text"
           id="brand"
           name="brand"
           defaultValue={product?.brand}
           required
+          className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-stone-900 outline-1 -outline-offset-1 outline-stone-300 focus:outline-2 focus:-outline-offset-2 focus:outline-violet-600 sm:text-sm/6"
         />
       </p>
-      <p>
-        <label htmlFor="price">Price</label>
+      <p className="flex flex-col gap-2">
+        <label
+          htmlFor="price"
+          className="block text-sm/6 font-medium text-stone-900"
+        >
+          Price
+        </label>
         <input
           type="text"
           id="price"
           name="price"
           defaultValue={product?.price}
           required
+          className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-stone-900 outline-1 -outline-offset-1 outline-stone-300 focus:outline-2 focus:-outline-offset-2 focus:outline-violet-600 sm:text-sm/6"
         />
       </p>
-      <p>
-        <label htmlFor="stock">Stock</label>
+      <p className="flex flex-col gap-2">
+        <label
+          htmlFor="stock"
+          className="block text-sm/6 font-medium text-stone-900"
+        >
+          Stock
+        </label>
         <input
           type="text"
           id="stock"
           name="stock"
           defaultValue={product?.stock}
           required
+          className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-stone-900 outline-1 -outline-offset-1 outline-stone-300 focus:outline-2 focus:-outline-offset-2 focus:outline-violet-600 sm:text-sm/6"
         />
       </p>
-      <p>
-        <button type="reset">Reset</button>
-        <button type="submit">Save</button>
+      <p className="flex justify-end gap-4">
+        <button
+          type="reset"
+          className="flex rounded-md border border-stone-200 px-4 py-2 text-sm font-semibold hover:border-stone-400"
+        >
+          Reset
+        </button>
+        <button
+          type="submit"
+          className="flex rounded-md border bg-violet-800 px-4 py-2 text-sm font-semibold text-white hover:bg-violet-900"
+        >
+          Save
+        </button>
       </p>
     </form>
   );

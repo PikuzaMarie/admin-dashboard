45ef4cdcec9a7566c5663b80ed231c97658fe53f
PikuzaMarie <frag1lemaster@proton.me>
2025-04-24 18:55:30 +0300

    refactor: extract routing logic into App component
    create ProtectedRoute to prevent unauthorized access to pages other than AuthPage

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,64 @@
-import { RouterProvider } from 'react-router-dom';
+import { Navigate } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
+import { ROUTES } from './constants';
+import { selectCurrentUserId } from './features/auth/authSlice';
 import { fetchUsers } from './features/users/usersSlice';
-import { useAppDispatch } from './hooks';
-import { routes } from './pages/router';
+import { useAppDispatch, useAppSelector } from './hooks';
+import { AuthenticationPage } from './pages/Authentication';
+import { CreateProductPage } from './pages/CreateProduct';
+import { EditProductPage } from './pages/EditProduct';
+import { HomePage } from './pages/Home';
+import { ProductPage } from './pages/Product';
+import { ProductsPage } from './pages/Products';
+import { RootLayout } from './pages/RootLayout';
+
+const ProtectedRoute = ({ children }: { children: React.ReactNode }) => {
+  const userId = useAppSelector(selectCurrentUserId);
+
+  if (userId === 0) {
+    return <Navigate to={ROUTES.auth} replace />;
+  }
+
+  return <RootLayout>{children}</RootLayout>;
+};
 
 function App() {
   const dispatch = useAppDispatch();
   dispatch(fetchUsers());
 
-  return <RouterProvider router={routes} />;
+  return (
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path={ROUTES.auth} element={<AuthenticationPage />} />
+          <Route
+            path="/*"
+            element={
+              <ProtectedRoute>
+                <Routes>
+                  <Route path={ROUTES.home} element={<HomePage />} />
+                  <Route path={ROUTES.products} element={<ProductsPage />} />
+                  <Route
+                    path={`${ROUTES.products}/:productId`}
+                    element={<ProductPage />}
+                  />
+                  <Route
+                    path={`${ROUTES.products}/:productId/edit`}
+                    element={<EditProductPage />}
+                  />
+                  <Route
+                    path={`${ROUTES.products}/new`}
+                    element={<CreateProductPage />}
+                  />
+                </Routes>
+              </ProtectedRoute>
+            }
+          />
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
--- src/pages/RootLayout.tsx
+++ src/pages/RootLayout.tsx
@@ -1,12 +1,12 @@
-import { Outlet } from 'react-router-dom';
-
 import { Sidebar } from '../components/Sidebar';
 
-export const RootLayout: React.FC = () => {
+export const RootLayout: React.FC<{ children: React.ReactNode }> = ({
+  children,
+}) => {
   return (
     <div className="grid grid-cols-[220px_1fr] gap-4 bg-stone-100 p-4 text-stone-950">
       <Sidebar />
-      <Outlet />
+      {children}
     </div>
   );
 };

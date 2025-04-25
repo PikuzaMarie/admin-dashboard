e1c968b98d9207b981fca86710bc806bfae97613
PikuzaMarie <frag1lemaster@proton.me>
2025-04-25 12:11:07 +0300

    feat: create 404 page and add a route to it

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Navigate } from 'react-router-dom';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 import { HomePage } from './components/HomePage';
+import { NotFoundPage } from './components/NotFoundPage';
 import { RootLayout } from './components/RootLayout';
 import { ROUTES } from './constants';
 import { AuthenticationPage } from './features/auth/AuthenticationPage';
@@ -47,6 +48,7 @@ function App() {
                     path={`${ROUTES.products}/new`}
                     element={<CreateProductPage />}
                   />
+                  <Route path="*" element={<NotFoundPage />} />
                 </Routes>
               </ProtectedRoute>
             }
---
+++ src/components/NotFoundPage.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import { ROUTES } from '../constants';
+import { Dashboard } from './UI/Dashboard';
+
+export const NotFoundPage: React.FC = () => {
+  return (
+    <Dashboard title="404 page">
+      <h3 className="text-5xl font-bold text-violet-500">404</h3>
+      <p className="text-base">
+        Couldn't find requested source or page. Return to Home Page, please :)
+      </p>
+      <Link
+        to={ROUTES.home}
+        className="flex w-fit items-center rounded-md border border-violet-800 p-2 text-sm font-semibold text-violet-800 hover:bg-violet-800 hover:text-violet-100"
+      >
+        Home Page
+      </Link>
+    </Dashboard>
+  );
+};
